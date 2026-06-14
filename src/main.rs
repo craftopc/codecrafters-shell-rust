a3ce2e799ac7c973b79d279ba729e92201fb136b
craftopc <lhon901@163.com>
2026-06-14 22:32:42 +0800

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -4,4 +4,15 @@ use std::io::{self, Write};
 fn main() {
     print!("$ ");
     io::stdout().flush().unwrap();
+
+    let mut command = String::new();
+    match io::stdin().read_line(&mut command) {
+        Ok(0) => println!("exit"),
+        Ok(_) => {
+            let command = command.trim();
+            println!("{}: command not found", command);
+        }
+        Err(e) => println!("shell: read error: {}", e)
+    }
+
 }
