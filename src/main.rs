df695ce932283d4ff6da6fd8c9aea2e29e161eed
craftopc <lhon901@163.com>
2026-06-14 22:49:05 +0800

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
+use std::process::exit;
 
 fn main() {
     loop {
@@ -14,9 +15,22 @@ fn main() {
             }
             Ok(_) => {
                 let command = command.trim();
-                println!("{}: command not found", command);
+                exec(command);
             }
             Err(e) => println!("shell: read error: {}", e),
+        }
+    }
+}
+
+fn exec(command: &str) {
+    match command {
+        "" => {}
+        "exit" => {
+            println!("exit");
+            exit(0);
+        }
+        _ => {
+            println!("{}: command not found", command);
         }
     }
 }
